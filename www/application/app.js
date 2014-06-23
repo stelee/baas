b5c29823bd351fe924f5d52c48553dbace4a36eb
unknown <yang.li@CANB29DYZ1.corp.ad.cginet>
2014-06-23 17:35:26 -0400

    working

--- www/application/app.js
+++ www/application/app.js
@@ -3,6 +3,14 @@
 	//global injection
 	//Always to be the first of app.js
 	var app=angular.module('restoApp',['ngRoute'])
+	
 	var router=require('application/libs/router.js').getInstance();
 	router.config(app);
+
+	app.directive('mpHeader',function(){
+		return {
+			template : '<h1>Welcome</h1>'
+		};
+	});
+	
 })(this)
