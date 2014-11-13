2e12faa186b2e01e3b74ddcf89e837220863fefd
unknown <yang.li@CANB29DYZ1.corp.ad.cginet>
2014-11-13 17:08:02 -0500

    fix some bug of the service more service need to be done in the near future

--- libs/opencart/products.js
+++ libs/opencart/products.js
@@ -7,6 +7,8 @@ var Nil=require('../utils/null');
 var mixin=require('../mixin').mixin;
 var withGetLanguageId=require('../comps/withGetLanguageId');
 
+var htmlEnDecode=require('../utils/htmlEnDecode');
+
 var SQLCONST={
 	'list_all': 'select \
 p.product_id as id, d.name as `name`, d.description,p.image as image_url,p.price as price, pdiscount.price as discount_price, pspecial.price as special_price \
@@ -17,16 +19,18 @@ left outer join oc_product_special as pspecial on pspecial.product_id = p.produc
 where p.stock_status_id =7 order by p.date_modified desc, p.viewed, p.sort_order asc \
 limit ? offset ?',
 	'list_by_category_id' : 'select \
-p.product_id as id, d.name as `name`, d.description,p.image as image_url,p.price as price, pdiscount.price as discount_price, pspecial.price as special_price \
-from oc_product as p \
-inner join oc_product_to_category as p2c on p2c.product_id=p.product_id and p2c.category_id =? \
-left outer join oc_product_description as d on d.product_id=p.product_id and d.language_id=? \
+distinct p.quantity as stock,p.product_id as id, d.name as `name`, d.description,p.image as image_url,p.price as price, pdiscount.price as discount_price, pspecial.price as special_price \
+from oc_product_to_category as p2c \
+inner join oc_product as p on p2c.product_id=p.product_id and p.stock_status_id =7 \
+inner join oc_product_description as d on d.product_id=p.product_id and d.language_id=? \
 left outer join oc_product_discount as pdiscount on pdiscount.product_id = p.product_id and now()>= pdiscount.date_start and now()<= pdiscount.date_end \
 left outer join oc_product_special as pspecial on pspecial.product_id = p.product_id and now()>= pspecial.date_start and now()<= pspecial.date_end \
-where p.stock_status_id =7 order by p.date_modified desc, p.viewed, p.sort_order asc \
+where \
+(p2c.category_id =? or p2c.category_id in(select category_id from oc_category where parent_id = ?)) \
+order by p.date_modified desc, p.viewed, p.sort_order asc \
 limit ? offset ?',
 	'product' : 'select \
-p.product_id as product_id, d.name as `name`, d.description,p.image as image_url,p.price as price, pdiscount.price as discount_price, pspecial.price as special_price \
+p.quantity as stock,p.product_id as product_id, d.name as `name`, d.description,p.image as image_url,p.price as price, pdiscount.price as discount_price, pspecial.price as special_price \
 from oc_product as p \
 left outer join oc_product_description as d on d.product_id=p.product_id and d.language_id=? \
 left outer join oc_product_discount as pdiscount on pdiscount.product_id = p.product_id and now()>= pdiscount.date_start and now()<= pdiscount.date_end \
@@ -72,7 +76,7 @@ Products.prototype.listByCategory=function(categoryId,language,recordsPerPage,cu
 	var sql=SQLCONST['list_by_category_id'];
 
 	return new Promise(function(resolve,reject){
-		database.query(sql,[categoryId,language_id,recordsPerPage,currentOffset]).then(function(data){
+		database.query(sql,[language_id,categoryId,categoryId,recordsPerPage,currentOffset]).then(function(data){
 			resolve(reformatData(data));
 		})
 	});
@@ -90,11 +94,16 @@ Products.prototype.product=function(productId,language)
 			}else
 			{
 				var ret=data[0];
-				if(Nil.isNotEmpty(ret.image_url)) ret.image_url = IMAGE_PREFIX_PATH + ret.image_url;
+				if(Nil.isNotEmpty(ret.image_url)) {
+					ret.image_url = IMAGE_PREFIX_PATH + ret.image_url.replace(/\.jpg$/g,"-500x500.jpg");
+					ret.cover=ret.image_url;
+				};
+				ret.currency='CAD';
+				ret.description=htmlEnDecode.htmlDecode(ret.description);
 				var sql2=SQLCONST['product_image'];
 				database.query(sql2,[ret.product_id]).then(function(rs){
 					ret.images=rs.map(function(elem){
-						return IMAGE_PREFIX_PATH  + elem.image
+						return IMAGE_PREFIX_PATH  + elem.image.replace(/\.jpg$/g,"-500x500.jpg");
 					});
 					resolve(ret);
 				})
@@ -109,8 +118,9 @@ var reformatData=function(data)
 	return data.map(
 				function(elem){
 					if(Nil.isNotEmpty(elem.image_url)){
-						elem.image_url=IMAGE_PREFIX_PATH + elem.image_url;
+						elem.image_url=IMAGE_PREFIX_PATH + elem.image_url.replace(/\.jpg$/g,"-200x200.jpg");
 					}
+					elem.description=htmlEnDecode.htmlDecode(elem.description);
 					return elem;
 			});
 }
